8e0e10f605bbd2666fd87f838549e25acddb4e26
雨落惊风 <1603385401@qq.com>
2019-05-04 13:12:09 +0800

    更新

--- src/main/java/lambdasinaction/chap8/OnlineBankingLambda.java
+++ src/main/java/lambdasinaction/chap8/OnlineBankingLambda.java
@@ -7,18 +7,52 @@ public class OnlineBankingLambda {
 
     public static void main(String[] args) {
         new OnlineBankingLambda().processCustomer(1337,
-                (Customer c) -> System.out.println("Hello!"));
+                (Customer c) -> System.out.println(c.getId() + " Hello " + c.getName()));
     }
 
-    public void processCustomer(int id, Consumer<Customer> makeCustomerHappy){
+    public void processCustomer(int id, Consumer<Customer> makeCustomerHappy) {
         Customer c = Database.getCustomerWithId(id);
         makeCustomerHappy.accept(c);
     }
 
-    /** dummy Customer class */
-    static private class Customer {}
-    /** dummy Database class */
-    static private class Database{
-        static Customer getCustomerWithId(int id){ return new Customer();}
+    /**
+     * dummy Customer class
+     */
+    static private class Customer {
+        private int id;
+        private String name;
+
+        public Customer() {
+        }
+
+        public Customer(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    /**
+     * dummy Database class
+     */
+    static private class Database {
+        static Customer getCustomerWithId(int id) {
+            return new Customer(id, "haha");
+        }
     }
 }
--- src/main/java/lambdasinaction/chap8/StrategyMain.java
+++ src/main/java/lambdasinaction/chap8/StrategyMain.java
@@ -7,7 +7,7 @@ public class StrategyMain {
         // old school
         Validator v1 = new Validator(new IsNumeric());
         System.out.println(v1.validate("aaaa"));
-        Validator v2 = new Validator(new IsAllLowerCase ());
+        Validator v2 = new Validator(new IsAllLowerCase());
         System.out.println(v2.validate("bbbb"));
 
 
@@ -24,23 +24,27 @@ public class StrategyMain {
 
     static private class IsAllLowerCase implements ValidationStrategy {
         @Override
-        public boolean execute(String s){
+        public boolean execute(String s) {
             return s.matches("[a-z]+");
         }
     }
+
     static private class IsNumeric implements ValidationStrategy {
         @Override
-        public boolean execute(String s){
+        public boolean execute(String s) {
             return s.matches("\\d+");
         }
     }
 
-    static private class Validator{
+    static private class Validator {
         private final ValidationStrategy strategy;
-        public Validator(ValidationStrategy v){
+
+        public Validator(ValidationStrategy v) {
             this.strategy = v;
         }
-        public boolean validate(String s){
-            return strategy.execute(s); }
+
+        public boolean validate(String s) {
+            return strategy.execute(s);
+        }
     }
 }
